cd3f89ebdb9f5973b3c0be3259daa17a94dd3d71
Kapdap <kapdap.nz@gmail.com>
2020-08-10 08:47:03 +1200

    Add Enemy types enumeration

---
+++ RECVXSRT/EnemyEnumeration.cs
@@ -0,0 +1,29 @@
+﻿namespace RECVXSRT
+{
+    public enum EnemyEnumeration : int
+    {
+        None = 0x00000000,
+        Unknown = 0x00306570,
+        Zombie = 0x00307A10,
+        ZombieB = 0x00307AA0,
+        GlupWorm = 0x0030D350,
+        BlackWidow = 0x0030E840,
+        ZombieDog = 0x0030F540,
+        Hunter = 0x00311E40,
+        Moth = 0x00312170,
+        Bat = 0x00312750,
+        Bandersnatch = 0x00313C90,
+        AlexiaAshford = 0x00317BE0,
+        AlexiaAshfordB = 0x00318CD0,
+        Nosferatu = 0x003196E0,
+        MutatedSteve = 0x0031A460,
+        AlbinoidAdult = 0x0031D3F0,
+        AlbinoidInfant = 0x0031C910,
+        GiantBlackWidow = 0x0031EED0,
+        BabyBlackWidow = 0x0031F6A0,
+        AnatomistZombie = 0x003203E0,
+        AlexiaBaby = 0x003211D0,
+        Tyrant = 0x00357950,
+        Tenticle = 0x003584D0
+    }
+}
