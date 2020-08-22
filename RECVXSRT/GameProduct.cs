cd89b1aa5d8d6776dd6f6ee98141ac4308c0e77c
Kapdap <kapdap.nz@gmail.com>
2020-08-22 23:24:56 +1200

    Add console system to product

--- RECVXSRT/GameProduct.cs
+++ RECVXSRT/GameProduct.cs
@@ -1,10 +1,13 @@
-﻿namespace RECVXSRT
+﻿using System;
+
+namespace RECVXSRT
 {
     public class GameProduct
     {
         public string Code { get; private set; }
         public string Name { get; private set; }
         public string Country { get; private set; }
+        public string System { get; private set; }
         public bool Supported { get; private set; }
 
         public const string SLPM_65022 = "SLPM_650.22";
@@ -28,37 +31,44 @@
         {
             Code = code;
             Supported = true;
+            System = String.Empty;
 
             switch (Code)
             {
                 case SLPM_65022:
                     Name = "BioHazard Code: Veronica Kanzenban";
                     Country = "JP";
+                    System = "PS2";
                     break;
 
                 case SLUS_20184:
                     Name = "Resident Evil Code: Veronica X";
                     Country = "US";
+                    System = "PS2";
                     break;
 
                 case SLES_50306:
                     Name = "Resident Evil Code: Veronica X";
                     Country = "EU";
+                    System = "PS2";
                     break;
 
                 case NPJB00135:
                     Name = "BioHazard Code: Veronica Kanzenban";
                     Country = "JP";
+                    System = "PS3";
                     break;
 
                 case NPUB30467:
                     Name = "Resident Evil Code: Veronica X HD";
                     Country = "US";
+                    System = "PS3";
                     break;
 
                 case NPEB00553:
                     Name = "Resident Evil Code: Veronica X";
                     Country = "EU";
+                    System = "PS3";
                     break;
 
                 default:
